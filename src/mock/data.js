a0d89aff6cb9606d750fa211313326537f2e4d90
marciorasf <marciorasf@gmail.com>
2021-05-22 16:19:35 -0300

    improve about text

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne:
     "I'm a software engineer currently working at Bume. My focus is to always become a better problem solver. Although I love programming, I think programming is valuable only if it can solve problems.",
   paragraphTwo:
-    "I have a solid Node.js + React.js stack knowledge combined to SQL or NoSQL databases. I also can also work with python and C. At the moment I'm focusing in learn the basics concepts about software engineering including: architecture, cloud computing, clean code, design patterns and deep OOP concepts.",
+    "I have a solid Node.js + React.js stack knowledge combined to SQL or NoSQL databases. I also can also work with python and C. At the moment I'm focused in learning software engineering concepts, including: architecture, cloud computing, clean code, design patterns and deep OOP concepts.",
   paragraphThree:
     "Talking about my education, I have a electrical engineering undergraduate degree focused on software engineering at UFMG. Complementarily to that I'm always following tutorials and studying topics that I consider important by my own.",
   resume: '', // if no resume, the button will not show up
