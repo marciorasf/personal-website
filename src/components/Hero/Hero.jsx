4b1f17cf63c32d2fb0d23d87900c2da793f8d320
marciorasf <marciorasf@gmail.com>
2021-05-25 21:31:51 -0300

    do not use mock on hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,20 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
-import PortfolioContext from '../../context/context';
+
+const data = {
+  title: "Hello, I'm ",
+  name: 'Márcio',
+  subtitle: '',
+  cta: 'Know more',
+};
 
 const Header = () => {
-  const { hero } = useContext(PortfolioContext);
-  const { title, name, subtitle, cta } = hero;
-
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+
+  const { title, name, subtitle, cta } = data;
 
   useEffect(() => {
     if (window.innerWidth > 769) {
@@ -26,17 +31,18 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={750} delay={250} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            {title}
+            <span className="text-color-main">{name}</span>
             <br />
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle}
           </h1>
         </Fade>
+
         <Fade left={isDesktop} bottom={isMobile} duration={750} delay={500} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                {cta}
               </Link>
             </span>
           </p>
